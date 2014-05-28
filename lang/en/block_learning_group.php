f11502992505cfa34688e03d4cb3e2c4c5923ce8
Matthias Baume <matthias.baume@tum.de>
2014-05-28 15:57:53 +0200

    Update block_learning_group.php
    Updated language file English.

--- lang/en/block_learning_group.php
+++ lang/en/block_learning_group.php
@@ -3,7 +3,7 @@
 $string['pluginname'] = 'Learning Group Block';
 $string['see_block_learning_group'] = 'Manage learning groups';
 $string['learning_group'] = 'Learning Group';
-$string['create_learning_group'] = 'create_learning_group';
+$string['create_learning_group'] = 'Create a new learning group';
 $string['learning_group:addinstance'] = 'Add a new learning group block';
 $string['learning_group:myaddinstance'] = 'Add a new learning group block to the My Moodle page';
 $string['version'] = 'Version: ';
@@ -11,73 +11,82 @@ $string['release'] = 'Release: ';
 $string['date_release'] = '{$a->month}.{$a->date}.{$a->year}';
 $string['mysessions'] = 'My sessions';
 $string['publicsessions'] = 'Public sessions';
-$string['bookingfull'] = 'bookingfull';
+$string['bookingfull'] = 'No free places left';
 $string['date'] = 'Date';
 $string['title'] = 'title';
 $string['time'] = 'Time';
 $string['capacity'] = 'Capacity';
-$string['capacity_help'] = 'capacity_help';
+$string['capacity_help'] = 'Total number of attendees possible';
 $string['status'] = 'Status';
-$string['unknowndate'] = 'unknowndate';
-$string['unknowntime'] = 'unknowntime';
+$string['unknowndate'] = 'Date unknown';
+$string['unknowntime'] = 'Time unknown';
 $string['options'] = 'Options';
-$string['previoussessionslist'] = 'previoussessionslist';
-$string['wait-listed'] = 'wait-listed';
-$string['bookingopen'] = 'bookingopen';
-$string['editsession'] = 'editsession';
-$string['edit'] = 'edit';
-$string['copy'] = 'copy';
-$string['copysession'] = 'copysession';
-$string['delete'] = 'delete';
-$string['deletesession'] = 'deletesession';
-$string['attendees'] = 'attendees';
-$string['seeattendees'] = 'seeattendees';
-$string['signup'] = 'signup';
-$string['addsession'] = 'addsession';
+$string['previoussessionslist'] = 'Previous sessions';
+$string['wait-listed'] = 'No fixed dates';
+$string['bookingopen'] = 'Free places';
+$string['editsession'] = 'Edit a session';
+$string['edit'] = 'Edit';
+$string['copy'] = 'Copy';
+$string['copysession'] = 'Copy a session';
+$string['delete'] = 'Delete';
+$string['deletesession'] = 'Delete a session';
+$string['attendees'] = 'Attendees';
+$string['seeattendees'] = 'Show attendees';
+$string['signup'] = 'Signup';
+$string['addsession'] = 'Add a session';
 $string['mylearninggrouptitle'] = 'My learning groups';
 $string['sessionsdetailstablesummary'] = 'Attendees';
-$string['sessiondatetime'] = 'sessiondatetime';
+$string['sessiondatetime'] = 'Session date and time';
 $string['details'] = 'Details';
-$string['details_help'] = 'Details Hilfe';
-$string['nosignedupusers'] = 'nosignedupusers';
-$string['addremoveattendees'] = 'addremoveattendees';
-$string['goback'] = 'goback';
-$string['modulenameplural'] = 'modulenameplural';
-$string['modulename'] = 'modulename';
-$string['potentialattendees'] = 'potentialattendees';
-$string['suppressemail'] = 'suppressemail';
-$string['attendeestablesummary'] = 'attendeestablesummary';
-$string['attendance'] = 'attendance';
-$string['noupcoming'] = 'noupcoming';
-$string['status_requested'] = 'status_requested';
-$string['unapprovedrequests'] = 'unapprovedrequests';
-$string['moreinfo'] = 'moreinfo';
-$string['cancelbooking'] = 'cancelbooking';
-$string['joinmeeting'] = 'joinmeeting';
-$string['status_booked'] = 'status_booked';
-$string['approve'] = 'approve';
-$string['sessiondatetimeknown'] = 'sessiondatetimeknown';
-$string['sessiondatetimeknown_help'] = '**If a session\'s date/time is known**';
-$string['datetimeknownhinttext'] = 'datetimeknownhinttext';
-$string['timestart'] = 'timestart';
-$string['timefinish'] = 'timefinish';
-$string['dateremove'] = 'dateremove';
-$string['dateadd'] = 'dateadd';
-$string['sessionover'] = 'sessionover';
-$string['bookingcancelled'] = 'bookingcancelled';
-$string['cancellationsentmgr'] = 'cancellationsentmgr';
-$string['cancelbooking'] = 'cancelbooking';
-$string['cancellationconfirm'] = 'cancellationconfirm';
-$string['cancelbookingfor'] = 'cancelbookingfor';
-$string['notificationboth'] = 'notificationboth';
-$string['notificationemail'] = 'notificationemail';
-$string['notificationical'] = 'notificationical';
-$string['notificationtype'] = 'notificationtype';
+$string['details_help'] = 'Details help';
+$string['nosignedupusers'] = 'Users without signup';
+$string['addremoveattendees'] = 'Edit attendees';
+$string['goback'] = 'Go back';
+$string['modulenameplural'] = 'Online Learning Groups';
+$string['modulename'] = 'Online Learning Group';
+$string['potentialattendees'] = 'Potential attendees';
+$string['suppressemail'] = 'Suppress email';
+$string['attendeestablesummary'] = 'Summary of attendees';
+$string['attendance'] = 'Attendees';
+$string['noupcoming'] = 'No upcoming learning group';
+$string['status_requested'] = 'Confirm invitation';
+$string['unapprovedrequests'] = 'Unapproved requests';
+$string['moreinfo'] = 'More info';
+$string['cancelbooking'] = 'Cancel booking';
+$string['joinmeeting'] = 'Join the meeting';
+$string['status_booked'] = 'Booked';
+$string['approve'] = 'Approve';
+$string['sessiondatetimeknown'] = 'Known session dates';
+$string['sessiondatetimeknown_help'] = 'If you have concrete session dates, you can enlist them.';
+$string['datetimeknownhinttext'] = 'You can fix your concrete session dates.';
+$string['timestart'] = 'Start time';
+$string['timefinish'] = 'End time';
+$string['dateremove'] = 'Remove date';
+$string['dateadd'] = 'Add date';
+$string['sessionover'] = 'Session over';
+$string['bookingcancelled'] = 'Booking cancelled';
+$string['cancellationsentmgr'] = 'Rejection';
+$string['cancelbooking'] = 'Cancel booking';
+$string['cancellationconfirm'] = 'Do you really want to cancel?';
+$string['cancelbookingfor'] = 'Cancel booking';
+$string['notificationboth'] = 'Mail and calender notification';
+$string['notificationemail'] = 'email notification';
+$string['notificationical'] = 'ical notification';
+$string['notificationtype'] = 'Notification type';
 $string['publiclearninggroup'] = 'Public learning group';
-$string['notificationtype_help'] = 'notificationtype_help';
-$string['deletesessionconfirm'] = 'deletesessionconfirm';
-$string['sessioninprogress'] = 'sessioninprogress';
+$string['notificationtype_help'] = 'You can be notified by email or calender';
+$string['deletesessionconfirm'] = 'Do you really want to delete the session?';
+$string['sessioninprogress'] = 'Session in progress';
 $string['manageremailheading'] = 'Manager Emails';
+$string['timerequested'] = 'Time requested';
+$string['decidelater'] = 'Decide later';
+$string['decline'] = 'Decline';
+$string['approve'] = 'Approve';
+$string['cancellations'] = 'Cancellations';
+$string['timesignedup'] = 'Time signup';
+$string['timecancelled'] = 'Time cancelled';
+$string['cancelreason'] = 'Cancel reason';
+$string['updaterequests'] = 'Update requests';
 $string['placeholder:learning_group_title'] = '[learning_group_title]';
 $string['placeholder:attendeeslink'] = '[attendeeslink]';
 $string['placeholder:firstname'] = '[firstname]';
@@ -104,16 +113,16 @@ To re-schedule your booking you need to cancel this booking and then re-book a n
 [details]
 
 ';
-$string['setting:defaultrequestinstrmngrdefault'] = 'Sie ([firstname] [lastname]) wurden gerade  in eine Lerngruppe eingeladen.
+$string['setting:defaultrequestinstrmngrdefault'] = 'You ([firstname] [lastname]) have been invited to a learning group.
 
-Lerngruppe:   [learning_group_title]
+Learning Group:   [learning_group_title]
 
-Termin(e):
+Date(s):
 [alldates]
 
 
 
-Sie können die Teilnahme über diesen Link bestätigen:
+You can confirm the invitation by clicking on the following link:
 [attendeeslink]#unapproved
 
 
